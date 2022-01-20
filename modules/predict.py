df03719a626c82b194e6132acc0b11bc2e9fe645
Lamhuyn <92633366+Lamhuyn@users.noreply.github.com>
2022-01-21 00:00:25 +0700

    1

--- modules/predict.py
+++ modules/predict.py
@@ -1,6 +1,6 @@
 import cv2 
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r"/usr/local/Cellar/tesseract/5.0.0/include/tesseract/"
+pytesseract.pytesseract.tesseract_cmd = r"/usr/local/Cellar/tesseract/5.0.0/share/tessdata/"
 def main(imgage):
     img= cv2.imread(imgage)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
