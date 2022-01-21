f749cdd3a77dd48612832281d21d8a8ab6f319e4
Lamhuyn <92633366+Lamhuyn@users.noreply.github.com>
2022-01-21 09:20:14 +0700

    1

--- streamlit.py
+++ streamlit.py
@@ -1,8 +1,9 @@
 import streamlit as st
 from PIL import Image
+import cv2 
+import numpy as np
+import pytesseract
 import os
-from modules.predict import main
-
 def load_image(image_file):
     img = Image.open(image_file)
     return img
@@ -16,7 +17,17 @@ if image_file is not None :
         f.write((image_file).getbuffer())
     click = st.button("Process")
     if  click:
+        pytesseract.pytesseract.tesseract_cmd = r"/usr/local/Cellar/tesseract/5.0.1/bin/tesseract"
         st.header("result: ")
         img=os.path.join("fileDir", image_file.name)
-        img=main(img)
+        img= cv2.imread(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        boxes=pytesseract.image_to_data(img)
+        for x,b in enumerate(boxes.splitlines()):
+            if x!=0 :
+                b=b.split()
+                if len(b)==12:
+                    x,y,w,h = int(b[6]) , int(b[7]) ,int(b[8]),int(b[9])
+                    cv2.rectangle(img, (x,y) , (x+w , y+h) , (0,0,255),2)
+                    cv2.putText(img , b[11] , (x,y) , cv2.FONT_HERSHEY_PLAIN,1,(50,50,255),1)
         st.image(img)
